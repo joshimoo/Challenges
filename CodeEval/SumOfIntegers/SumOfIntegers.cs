1035b213c14c44b40a4de56ea64acca1fb3b769c
Joshua Moody <joshimoo@hotmail.de>
2014-09-05 18:06:17 +0200

    Implemented solution for CodeEval's Sum of Integers from file challenge https://www.codeeval.com/open_challenges/24/

---
+++ CodeEval/SumOfIntegers/SumOfIntegers.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+
+namespace CodeEval.SumOfIntegers
+{
+    /// <summary>
+    /// Sum of Integers Challenge
+    /// Description: Print out the sum of integers read from a file
+    /// Problem Statement: https://www.codeeval.com/open_challenges/24/
+    /// </summary>
+    class FizzBuzz
+    {
+        /// <summary>
+        /// Entry Point for the Challenge
+        /// </summary>
+        /// <param name="args">Command line Arguments</param>
+        static void Main(string[] args)
+        {
+            string[] lines = File.ReadAllLines(args[0]);
+            int sum = 0;
+            foreach (var line in lines) { sum += int.Parse(line); }
+            Console.WriteLine(sum);
+        }
+
+        /* Line by line reading exmaple
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            using (StreamReader reader = File.OpenText(args[0]))
+            {
+                while (!reader.EndOfStream)
+                {
+                    string line = reader.ReadLine();
+                    if (!String.IsNullOrEmpty(line)) { sum += int.Parse(line); }
+                }
+            }
+         
+            Console.WriteLine(sum);
+        } */
+    }
+}
