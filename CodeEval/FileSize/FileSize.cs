b5b2ed533e7f5151022d1578c112ec1c06fafc9a
Joshua Moody <joshimoo@hotmail.de>
2014-09-05 18:18:44 +0200

    Implemented solution for CodeEval's File Size challenge https://www.codeeval.com/open_challenges/26/
    I used the FileInfo Class instead of manually reading the file size.

---
+++ CodeEval/FileSize/FileSize.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+namespace CodeEval.FileSize
+{
+    /// <summary>
+    /// File Size Challenge
+    /// Description: Print the size of a file in bytes
+    /// Problem Statement: https://www.codeeval.com/open_challenges/26/
+    /// </summary>
+    class FileSize
+    {
+        /// <summary>
+        /// Entry Point for the Challenge
+        /// </summary>
+        /// <param name="args">Command line Arguments</param>
+        static void Main(string[] args)
+        {
+            // FileInfo Class takes care of doing this for us :)
+            var fileInfo = new FileInfo(args[0]);
+            Console.WriteLine(fileInfo.Length);
+        }
+
+    }
+}
