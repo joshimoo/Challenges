742282bc01188b0ceb13bee4eec1282a56145136
Joshua Moody <joshimoo@hotmail.de>
2014-09-05 18:27:06 +0200

    Implemented solution for CodeEval's Odd Number challenge https://www.codeeval.com/open_challenges/25/

---
+++ CodeEval/OddNumbers/OddNumbers.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace CodeEval.OddNumbers
+{
+    /// <summary>
+    /// Odd Numbers Challenge
+    /// Description: Print the odd numbers from 1 to 99
+    /// Problem Statement: https://www.codeeval.com/open_challenges/25/
+    /// </summary>
+    class OddNumbers
+    {
+        /// <summary>
+        /// Entry Point for the Challenge
+        /// </summary>
+        /// <param name="args">Command line Arguments</param>
+        static void Main(string[] args)
+        {
+            // No input in this challenge
+            for (int i = 1; i <= 99; i += 2)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+    }
+}
