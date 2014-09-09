b4f7cb0eef26eebad4bbd2d2d7e29ce3c38e8280
Joshua Moody <joshimoo@hotmail.de>
2014-09-09 18:38:46 +0200

    Implemented solution for CodeEval's Multiples of a Number challenge https://www.codeeval.com/open_challenges/18/

---
+++ CodeEval/Easy/MultiplesOfANumber/MultiplesOfANumber.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace CodeEval.MultiplesOfANumber
+{
+    /// <summary>
+    /// Multiples of a Number Challenge
+    /// Difficulty: Easy
+    /// Description: print out the smallest multiple of n which is greater than or equal to x. Do not use division or modulo operator
+    /// Problem Statement: https://www.codeeval.com/open_challenges/18/
+    /// </summary>
+    class MultiplesOfANumber
+    {
+        /// <summary>
+        /// Entry Point for the Challenge
+        /// </summary>
+        /// <param name="args">Command line Arguments</param>
+        public static void Main(string[] args)
+        {
+            string[] lines = File.ReadAllLines(args[0]);
+            foreach (var line in lines)
+            {
+                string[] split = line.Split(',');
+                int target = int.Parse(split[0]);
+                int n = int.Parse(split[1]);
+                int sum = n;
+
+                while (sum < target) { sum += n; }
+                Console.WriteLine(sum);
+            }
+        }
+    }
+}
