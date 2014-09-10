b7bf88ab4c894b036b407175d20ba7294ec68e97
Joshua Moody <joshimoo@hotmail.de>
2014-09-10 13:30:16 +0200

    Implemented solution for HackerRanks Sherlock And Squares challenge https://www.hackerrank.com/challenges/sherlock-and-squares

---
+++ HackerRank/Algorithms/Warmup/SherlockAndSquares/SherlockAndSquares.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace HackerRank.Algorithms.Warmup.SherlockAndSquares
+{
+    /// <summary>
+    /// Sherlock and Squares Challenge
+    /// Description: Given two integers A & B count the number of square integers between A and B (both inclusive)
+    /// Problem Statement: https://www.hackerrank.com/challenges/sherlock-and-squares
+    /// </summary>
+    class SherlockAndSquares
+    {
+        public static void Main(string[] args)
+        {
+            string line = Console.ReadLine().Trim();
+            int t = int.Parse(line);
+            Debug.Assert(t >= 1 && t <= 100);
+            for (var i = 0; i < t; i++)
+            {
+                line = Console.ReadLine().Trim();
+                var split = line.Split(' ');
+                int a = int.Parse(split[0]);
+                int b = int.Parse(split[1]);
+                Debug.Assert(1 <= a && a <= b && b <= 1000000000);
+
+                // create a list so we don't have to recalculate already calculated entries
+                var calculatedSquares = new List<int>() { 0 };
+                Console.WriteLine(CalculateSquares(a, b, calculatedSquares));
+            }
+        }
+
+        static int CalculateSquares(int lowerBound, int upperBound, List<int> calculatedSquares)
+        {
+            int count = 0;
+
+            // Start from the last calculated square and see if its inside the search area
+            for (int i = calculatedSquares.Count; i * i <= upperBound; i++)
+            {
+                calculatedSquares.Add(i * i);
+            }
+
+            // Find all elements inside the search area
+            // Alternativly Use BinarySearch to find the optimal starting index
+            int startIndex = (int)Math.Ceiling(Math.Sqrt(lowerBound));
+            for (int i = startIndex; i * i <= upperBound; i++)
+            {
+                count++;
+            }
+
+            return count;
+        }
+
+    }
+}
