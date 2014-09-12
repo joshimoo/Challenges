0d80cafc9fca9a3dbab63c0fd3ef35fbe3d02fcf
Joshua Moody <joshimoo@hotmail.de>
2014-09-12 21:51:22 +0200

    Implemented solution for CodeEval's Longest Word challenge https://www.codeeval.com/open_challenges/111/

---
+++ CodeEval/Easy/LongestWord/LongestWord.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+
+namespace CodeEval.LongestWord
+{
+    /// <summary>
+    /// Longest Word Challenge
+    /// Difficulty: Easy
+    /// Description: Find the Longest Word in a sentence
+    /// Problem Statement: https://www.codeeval.com/open_challenges/111/
+    /// </summary>
+    class LongestWord
+    {
+        /// <summary>
+        /// Entry Point for the Challenge
+        /// </summary>
+        /// <param name="args">Command line Arguments</param>
+        public static void Main(string[] args)
+        {
+            string[] lines = File.ReadAllLines(args[0]);
+            foreach (var line in lines)
+            {
+                Console.WriteLine(FindLongestWord(line));
+            }
+        }
+
+        static string FindLongestWord(string sentence)
+        {
+            string longestWord = String.Empty;
+            string[] words = sentence.Split(' ');
+
+            foreach (string word in words)
+            {
+                if (word.Length > longestWord.Length)
+                {
+                    longestWord = word;
+                }
+            }
+
+            return longestWord;
+        }
+
+    }
+}
