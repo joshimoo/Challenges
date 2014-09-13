d237be8329a40ace4344bf6a7ea6a04e8a7a994b
Joshua Moody <joshimoo@hotmail.de>
2014-09-13 13:10:00 +0200

    Implemented solution for CodeEval's Lower Case challenge https://www.codeeval.com/open_challenges/20/

---
+++ CodeEval/Easy/Lowercase/Lowercase.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+
+namespace CodeEval.Lowercase
+{
+    /// <summary>
+    /// Lowercase Challenge
+    /// Difficulty: Easy
+    /// Description: Print the english(asci) text in Lowercase formatting
+    /// Problem Statement: https://www.codeeval.com/open_challenges/20/
+    /// </summary>
+    class Lowercase
+    {
+        /// <summary>
+        /// Entry Point for the Challenge
+        /// </summary>
+        /// <param name="args">Command line Arguments</param>
+        public static void Main(string[] args)
+        {
+            string[] lines = File.ReadAllLines(args[0]);
+            foreach (var line in lines)
+            {
+                char[] output = new char[line.Length];
+                for (int i = 0; i < line.Length; i++)
+                {
+                    char c = line[i];
+
+                    // The case flag, is bit 6 (n^5 = 32) in asci
+                    // upper case: bit 6 = 0
+                    // lower case: bit 6 = 1
+                    if (c >= 'A' && c <= 'Z') { c = (char)(c | 32); }
+                    output[i] = c;
+                }
+
+                Console.WriteLine(new string(output));
+            }
+        }
+
+    }
+}
