b95fb6962dcb7d2f541db0cd2c1f163e3c31c9d5
Joshua Moody <joshimoo@hotmail.de>
2014-09-16 00:20:20 +0200

    Implemented solution for CodeEval's Rightmost Char challenge https://www.codeeval.com/open_challenges/31/

---
+++ CodeEval/Easy/RightmostChar/RightmostChar.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+
+namespace CodeEval.RightmostChar
+{
+    /// <summary>
+    /// Rightmost Char Challenge
+    /// Difficulty: Easy
+    /// Description: return last index of a searched char inside of a string.
+    /// Problem Statement: https://www.codeeval.com/open_challenges/31/
+    /// </summary>
+    class RightmostChar
+    {
+        /// <summary>
+        /// Entry Point for the Challenge
+        /// </summary>
+        /// <param name="args">Command line Arguments</param>
+        public static void Main(string[] args)
+        {
+            using (StreamReader reader = File.OpenText(args[0]))
+            {
+                while (!reader.EndOfStream)
+                {
+                    string line = reader.ReadLine();
+                    if (!String.IsNullOrEmpty(line))
+                    {
+                        // Process line
+                        var split = line.Split(',');
+                        string sentence = split[0];
+                        char characterToFind = split[1][0];
+
+                        // iterate the string from behind, and set the result (initial value = -1) to i when you find a matching character then break the iteration and return result.
+                        Console.WriteLine(sentence.LastIndexOf(characterToFind));
+                    }
+                }
+            }
+        }
+
+    }
+}
