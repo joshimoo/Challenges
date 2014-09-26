bd574c2e3d8d4d595f78dbb321765b74ffe9e3b6
Joshua Moody <joshimoo@hotmail.de>
2014-09-26 22:32:27 +0200

    Implemented solution for CodeEval's Endianness challenge https://www.codeeval.com/open_challenges/15/

---
+++ CodeEval/Easy/Endianness/Endianness.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace CodeEval.Endianness
+{
+    /// <summary>
+    /// Endianness Challenge
+    /// Difficulty: Easy
+    /// Description: Return wether the system is Little Or BigEndianness
+    /// Problem Statement: https://www.codeeval.com/open_challenges/15/
+    /// </summary>
+    class Endianness
+    {
+        /// <summary>
+        /// Entry Point for the Challenge
+        /// </summary>
+        /// <param name="args">Command line Arguments</param>
+        public static void Main(string[] args)
+        {
+            // System Endian Flag: http://msdn.microsoft.com/en-us/library/system.bitconverter.islittleendian(v=vs.110).aspx
+            Console.WriteLine(BitConverter.IsLittleEndian ? "LittleEndian" : "BigEndian");
+        }
+    }
+}
