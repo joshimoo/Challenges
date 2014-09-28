b3269f6abb1665de9b4a5584bd082c708de0ab3b
Joshua Moody <joshimoo@hotmail.de>
2014-09-28 20:49:06 +0200

    Implemented solution for CodeEval's Unique Elements challenge https://www.codeeval.com/open_challenges/29/

---
+++ CodeEval/Easy/UniqueElements/UniqueElements.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace CodeEval.UniqueElements
+{
+    /// <summary>
+    /// Unique Elements Challenge
+    /// Difficulty: Easy
+    /// Description: Only print out unique elements
+    /// Problem Statement: https://www.codeeval.com/open_challenges/29/
+    /// </summary>
+    class UniqueElements
+    {
+        /// <summary>
+        /// Entry Point for the Challenge
+        /// </summary>
+        /// <param name="args">Command line Arguments</param>
+        public static void Main(string[] args)
+        {
+            string[] lines = File.ReadAllLines(args[0]);
+            foreach (var line in lines)
+            {
+                string[] split = line.Split(',');
+                StringBuilder sb = new StringBuilder();
+
+                string lastAdded = String.Empty;
+                for (int i = 0; i < split.Length; i++)
+                {
+                    if (split[i] != lastAdded)
+                    {
+                        sb.AppendFormat("{0},", split[i]);
+                        lastAdded = split[i];
+                    }
+                }
+
+                Console.WriteLine(sb.ToString(0, sb.Length - 1));
+            }
+        }
+
+    }
+}
