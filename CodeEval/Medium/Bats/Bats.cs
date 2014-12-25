54de3a2ee68adccc30eb9c293c42d6ad0c0f261e
Joshua Moody <joshimoo@hotmail.de>
2014-12-25 16:49:44 +0100

    Implemented solution for CodeEval's Bats challenge https://www.codeeval.com/open_challenges/146/

---
+++ CodeEval/Medium/Bats/Bats.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+
+namespace CodeEval.Bats
+{
+    /// <summary>
+    /// Bats Challenge
+    /// Difficulty: Medium
+    /// Description: Calculate how many additional bats can fit on the wire
+    /// Problem Statement: https://www.codeeval.com/open_challenges/146/
+    /// </summary>
+    class Bats
+    {
+        /// <summary>
+        /// Entry Point for the Challenge
+        /// </summary>
+        /// <param name="args">Command line Arguments</param>
+        public static void Main(string[] args)
+        {
+            foreach (var line in File.ReadLines(args[0]))
+            {
+                var split = Array.ConvertAll(line.Split(' '), int.Parse);
+                int wire = split[0];
+                int distance = split[1]; // distance beetwen bats, bats need to be 6 units from the buildings
+                int initialBatCount = split[2];
+                if (initialBatCount > 0) { Array.Sort(split, 3, initialBatCount); }
+                
+                // Bats need to be 6 units from the building
+                int pos = 6;
+                int count = 0;
+                int i = 3;
+
+                // TODO: Redo the Bats challenge, I don't like the way I solved it.
+                while (pos <= wire - 6)
+                {
+                    int bat = i < split.Length ? split[i] : pos + distance;
+                    if (pos <= bat - distance)
+                    {
+                        // I am in front of the current Bat
+                        count++;
+                        pos += distance;
+                    }
+                    else
+                    {
+                        // no more space in front of this bat
+                        pos = bat + distance;
+                        i++;
+                    }
+                }
+
+                Console.WriteLine(count);
+            }
+        }
+
+    }
+}
--- CodeEval/Program.cs
+++ CodeEval/Program.cs
@@ -7,8 +7,8 @@ namespace CodeEval
         static void Main(string[] args)
         {
             // Test Run
-            var input = new string[] { "Hard/ClosestPair/input.txt" };
-            ClosestPair.ClosestPair.Main(input);
+            var input = new string[] { "Medium/Bats/input.txt" };
+            Bats.Bats.Main(input);
 
             Console.WriteLine("Press any key to continue.");
             Console.ReadLine();
