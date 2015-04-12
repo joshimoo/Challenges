e904f2ea3d68f49c9f91face4bc0dd4e105972b4
Joshua Moody <joshimoo@hotmail.de>
2015-04-12 21:41:26 +0200

    Implemented solution for CodeEval's Minimum Distance challenge https://www.codeeval.com/open_challenges/189/

---
+++ CodeEval/Easy/MinimumDistance/MinimumDistance.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace CodeEval.MinimumDistance
+{
+    /// <summary>
+    /// Minimum Distance Challenge
+    /// Difficulty: Easy
+    /// Find the sum of the minimal distance to reach all of alices friends. (optimal position)
+    /// Problem Statement: https://www.codeeval.com/open_challenges/189/
+    /// </summary>
+    class MinimumDistance
+    {
+        /// <summary>
+        /// Entry Point for the Challenge
+        /// </summary>
+        /// <param name="args">Command line Arguments</param>
+        public static void Main(string[] args)
+        {
+            foreach (var line in File.ReadLines(args[0]))
+            {
+                // For O(n) implement Pivot Selection Algorithm
+                int[] numbers = line.Split(' ').Skip(1).Select(int.Parse).OrderBy(x => x).ToArray();
+
+                // median average
+                int mid = numbers.Length / 2;
+                int median = numbers.Length % 2 == 0 ? (numbers[mid] + numbers[mid - 1]) / 2 : numbers[mid];
+                int minDistance = numbers.Sum(num => Math.Abs(num - median));
+
+                Console.WriteLine(minDistance);
+            }
+
+        }
+
+    }
+}
