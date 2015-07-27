21c149d9d25ab82537f13efc4c76bd51cde815c9
Joshua Moody <joshimoo@hotmail.de>
2015-07-27 23:27:57 +0200

    Implemented solution for CodeEval's Clean up the words challenge https://www.codeeval.com/open_challenges/205/

---
+++ CodeEval/Easy/CleanUpTheWords/CleanUpTheWords.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace CodeEval.CleanUpTheWords
+{
+    /// <summary>
+    /// Clean up the words Challenge
+    /// Difficulty: Easy
+    /// Description: Clean up the noise beetwen 2 words
+    /// Problem Statement: https://www.codeeval.com/open_challenges/205/
+    /// </summary>
+    class CleanUpTheWords
+    {
+        /// <summary>
+        /// Entry Point for the Challenge
+        /// </summary>
+        /// <param name="args">Command line Arguments</param>
+        public static void Main(string[] args)
+        {
+            foreach (var line in File.ReadLines(args[0]))
+            {
+                StringBuilder sb = new StringBuilder();
+                bool newWord = false;
+                bool firstWord = true;
+
+                foreach (char c in line)
+                {
+                    // (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')
+                    if (Char.IsLetter(c))
+                    {
+                        // Add Space beetwen words
+                        if (newWord && !firstWord) { sb.Append(' '); }
+
+                        sb.Append(Char.ToLower(c));
+                        firstWord = false;
+                        newWord = false;
+                    }
+                    else 
+                    {
+                        newWord = true;
+                    }
+                }
+
+                Console.WriteLine(sb.ToString());
+            }
+        }
+
+    }
+}
