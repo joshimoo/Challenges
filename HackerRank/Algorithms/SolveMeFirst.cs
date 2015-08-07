6a6b79f9e99c7dce4dde9f14040eaacecdf35428
Joshua Moody <joshimoo@hotmail.de>
2015-08-07 22:44:55 +0200

    Implemented solution for HackerRanks Solve me First challenge https://www.hackerrank.com/challenges/solve-me-first

---
+++ HackerRank/Algorithms/SolveMeFirst.cs
@@ -0,0 +1,25 @@
+using System;
+
+namespace HackerRank.Algorithms
+{
+    /// <summary>
+    /// Solve Me First Challenge
+    /// Description: This is mainly used as an io template
+    /// Problem Statement: https://www.hackerrank.com/challenges/solve-me-first
+    /// </summary>
+    class SolveMeFirst
+    {
+        public static void Main(string[] args)
+        {
+            int leftOperand = Convert.ToInt32(Console.ReadLine());
+            int rightOperand = Convert.ToInt32(Console.ReadLine());
+            int sum = Sum(leftOperand, rightOperand);
+            Console.WriteLine(sum);
+        }
+
+        static int Sum(int leftOperand, int rightOperand)
+        {
+            return leftOperand + rightOperand;
+        }
+    }
+}
