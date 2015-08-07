89553e497a86a031b4276485f5cb18f7ff2b5032
Joshua Moody <joshimoo@hotmail.de>
2015-08-07 22:47:00 +0200

    Implemented solution for HackerRanks A very big sum challenge https://www.hackerrank.com/challenges/a-very-big-sum

---
+++ HackerRank/Algorithms/AVeryBigSum.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Linq;
+
+namespace HackerRank.Algorithms
+{
+    /// <summary>
+    /// A Very Big Sum Challenge
+    /// Description: Sum Big Numbers
+    /// Problem Statement: https://www.hackerrank.com/challenges/a-very-big-sum
+    /// </summary>
+    class AVeryBigSum
+    {
+        public static void Main(string[] args)
+        {
+            int count = Convert.ToInt32(Console.ReadLine());
+            long sum = Console.ReadLine().Split(' ').Sum(s => long.Parse(s));
+            Console.WriteLine(sum);
+        }
+    }
+}
