7b7408d4f0bcf85908171c98e84594f76267f5fd
Joshua Moody <joshimoo@hotmail.de>
2015-08-08 18:40:34 +0200

    Implemented solution for HackerRanks Caesar Cipher challenge
    Apply a Caesar Cipher but only to the chars a-z & A-Z
    https://www.hackerrank.com/challenges/caesar-cipher-1

---
+++ HackerRank/Algorithms/CaesarCipher.cs
@@ -0,0 +1,42 @@
+using System;
+
+namespace HackerRank.Algorithms
+{
+    /// <summary>
+    /// Caesar Cipher Challenge
+    /// Description: Apply a Caesar Cipher but only to the chars a-z & A-Z
+    /// Problem Statement: https://www.hackerrank.com/challenges/caesar-cipher-1
+    /// </summary>
+    class CaesarCipher
+    {
+        public static void Main(string[] args)
+        {
+            int length = Convert.ToInt32(Console.ReadLine());
+            string msg = Console.ReadLine();
+            int rot = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(Encode(msg, rot));
+        }
+
+
+        static string Encode(string msg, int rot)
+        {
+            var encoded = new char[msg.Length];
+            for (int i = 0; i < encoded.Length; i++)
+            {
+                char c = msg[i];
+                if (c >= 'a' && c <= 'z')
+                {
+                    c = (char)((((c - 'a') + rot) % 26) + 'a');
+                }
+                else if (c >= 'A' && c <= 'Z')
+                {
+                    c = (char)((((c - 'A') + rot) % 26) + 'A');
+                }
+
+                encoded[i] = c;
+            }
+
+            return new string(encoded);
+        }
+    }
+}
