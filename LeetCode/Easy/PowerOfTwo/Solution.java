6dbd87a0c98a61c858cfe3aa0de1fb02d83c1485
Joshua Moody <joshimoo@hotmail.de>
2016-05-04 12:56:10 +0200

    Fixed LeetCode: 231 - Power of Two

--- LeetCode/Easy/PowerOfTwo/Solution.java
+++ LeetCode/Easy/PowerOfTwo/Solution.java
@@ -11,12 +11,13 @@ public class Solution {
 
         // we just count the high bits
         // to be a power of two, only 1 bit must be set
-        // we use a logical right shift >>> to treat the number as unsigned
+        // we can use an arithmetical right shift >> that way negative numbers will always have a count > 1
+        // we could also check that the number is > 0 before entering the loop
         // we exit early, once we know it's no longer a power of two
         int count = 0;
-        while(n != 0 && count < 2) {
+        while(n > 0 && count < 2) {
             count += (n & 0x01);
-            n = n >>> 1;
+            n = n >> 1;
         }
 
         return count == 1;
